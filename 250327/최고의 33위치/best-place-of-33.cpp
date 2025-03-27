ccde626fb8ce5910b7851fc54b446cd7f70d68f5
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2025-03-27 17:19:43 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250327/최고의 33위치/best-place-of-33.cpp
+++ 250327/최고의 33위치/best-place-of-33.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int N;
 int grid[20][20];
+int ans;
+
+bool possible(int i, int j){
+    return j + 3 <= N && i + 3 <= N;
+}
+
+int calcaulate(int i, int j){
+    int cnt = 0;
+    for(int y = i; y < i + 3; y++){
+        for(int x = j ; x < j + 3; x++){
+            if(grid[y][x] == 1){
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
 
 int main() {
     cin >> N;
@@ -14,7 +31,18 @@ int main() {
         }
     }
 
-    // Please write your code here.
+    for(int i = 0; i< N; i++){
+        for(int j = 0; j < N ; j++){
+            if(!possible(j, i)){
+                continue;
+            }
+            int cur = calcaulate(i,j);
+            ans = max(ans, cur);
 
+        }
+    }
+
+    cout << ans;
     return 0;
 }
+
