a6f35097a6ce8e9d855064247e683d70cd87e97c
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2025-04-28 16:27:27 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250428/사각형 채우기 3/rectangle-fill-3.cpp
+++ 250428/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 
+#define MAX_N 1000
+#define MOD 1000000007
+
 using namespace std;
 
+// 변수 선언
 int n;
-int dp[1001];
+
+long long dp[MAX_N + 1];
 
 int main() {
+    // 입력:
     cin >> n;
 
+    // 초기 조건 설정
     dp[0] = 1;
     dp[1] = 2;
 
-
-    for(int i = 2; i <= n; i++){
-
-        dp[i] = (dp[i-1]*2 + dp[i-2] * 3) % 1000000007;
-
-        for(int j = i -3 ; j>=0; j--){
-            dp[i] = (dp[i] + (2 * dp[j])) % 1000000007;
-        }
-
+    // 점화식에 따라 dp값 채우기
+    // dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3 +
+    //         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
+    for(int i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+        for(int j = i - 3; j >= 0; j--)
+            dp[i] = (dp[i] + dp[j] * 2) % MOD;
     }
+    
     cout << dp[n];
-
     return 0;
 }
