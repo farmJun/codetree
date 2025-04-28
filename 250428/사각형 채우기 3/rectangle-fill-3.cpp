9a3045f462fe0ea93336d92c36888e3bcc557992
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2025-04-28 16:23:25 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250428/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+    cin >> n;
+
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 7;
+
+
+    for(int i = 3; i <= n; i++){
+
+        dp[i] = (dp[i-1]*2 + dp[i-2] * 3) % 10007;
+
+        for(int j = i -3 ; j>=0; j--){
+            dp[i] = (dp[i] + 2 * dp[j]) % 10007;
+        }
+
+
+
+    }
+    cout << dp[n];
+
+    return 0;
+}
