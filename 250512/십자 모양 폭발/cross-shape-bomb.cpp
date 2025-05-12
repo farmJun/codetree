ff2094ca37b25f2b9914f760f20ea30a68a9cb4e
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2025-05-12 15:34:13 +0900

    Title: 십자 모양 폭발, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250512/십자 모양 폭발/cross-shape-bomb.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[200][200];
+int r, c;
+
+bool isIn(int y, int x){
+    return y >= 0 && y < n && x >= 0 && x < n;
+}
+
+void print(){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << grid[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+}
+
+void gravity(){
+     for (int i = n-2; i > 0; i--) {
+        for (int j = 0; j < n; j++) {
+
+            if(grid[i-1][j] != 0 && grid[i][j] == 0){
+                grid[i][j] = grid[i-1][j];
+                grid[i-1][j] = 0;
+            }
+        }
+    }
+}
+
+void bomb(int y, int x){
+    int count = grid[y][x];
+    grid[y][x] = 0;
+
+    // 위 아래로
+    for(int i = 1; i < count; i++){
+        if(isIn(y + i, x)){
+            grid[y + i][x] = 0;
+        }
+
+        if(isIn(y - i, x)){
+            grid[y - i][x] = 0;
+        }
+
+        if(isIn(y, x + i)){
+            grid[y][x+i]=0;
+        }
+
+        if(isIn(y, x-i)){
+            grid[y][x-i] = 0;
+        }
+    }
+}
+
+
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    cin >> r >> c;
+
+    bomb(r-1,c-1);
+
+    gravity();
+    print();
+
+    return 0;
+}
